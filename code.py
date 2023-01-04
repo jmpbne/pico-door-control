f99823494a3a2c8f50ec3ba9561b093a7b8b9937
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-01-04 23:26:59 +0100

    Use plain functions instead of classes (for now)

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 import board
+import displayio
 from busio import I2C
-from displayio import Group, I2CDisplay, release_displays
+from displayio import Group, I2CDisplay
 
-from adafruit_bitmap_font.bitmap_font import load_font
+from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text.label import Label
 from adafruit_displayio_sh1106 import SH1106
 
@@ -17,44 +18,51 @@ FONT_FILENAME = "/bizcat.pcf"
 FONT_HEIGHT = 16
 
 
-class Display:
-    def __init__(self) -> None:
-        release_displays()
+def init_display():
+    displayio.release_displays()
 
-        i2c = I2C(DISPLAY_SCL, DISPLAY_SDA)
-        bus = I2CDisplay(i2c, device_address=DISPLAY_ADDRESS)
+    i2c = I2C(DISPLAY_SCL, DISPLAY_SDA)
+    bus = I2CDisplay(i2c, device_address=DISPLAY_ADDRESS)
 
-        self.display = SH1106(
-            bus,
-            width=DISPLAY_WIDTH,
-            height=DISPLAY_HEIGHT,
-            colstart=DISPLAY_OFFSET_X,
-            auto_refresh=False
-        )
+    display = SH1106(
+        bus,
+        width=DISPLAY_WIDTH,
+        height=DISPLAY_HEIGHT,
+        colstart=DISPLAY_OFFSET_X,
+        auto_refresh=False
+    )
 
-        self.display.show(Group())
-        self.display.auto_refresh = True
+    display.show(Group())
+    display.auto_refresh = True
 
-        self.font = load_font(FONT_FILENAME)
+    return display
 
-    def update(self, text: str) -> None:
-        group = Group()
 
-        for idx, text_line in enumerate(text.splitlines()):
-            text_area = Label(self.font, text=text_line, color=0xFFFFFF)
-            text_area.x = 0
-            text_area.y = (FONT_HEIGHT // 2) + FONT_HEIGHT * idx
+def update_display(display, font, text_lines):
+    group = Group()
 
-            group.append(text_area)
+    for idx, text_line in enumerate(text_lines):
+        text_area = Label(font, text=text_line, color=0xFFFFFF)
+        text_area.x = 0
+        text_area.y = (FONT_HEIGHT // 2) + FONT_HEIGHT * idx
 
-        self.display.show(group)
+        group.append(text_area)
+
+    display.show(group)
 
 
 if __name__ == "__main__":
-    text = "Motor avg speed:\n20%\n0123456789:01\n S+    S-    OK "
+    text_lines = [
+        "Motor avg speed:",
+        "20%",
+        "0123456789:01",
+        " S+    S-    OK",
+    ]
 
-    display = Display()
-    display.update(text)
+    font = bitmap_font.load_font(FONT_FILENAME)
+
+    display = init_display()
+    update_display(display, font.format(), text_lines)
 
     while True:
         pass
