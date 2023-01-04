6127c9bee2bf1a659008667f01db3fc4d6b4c69f
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-01-04 01:20:12 +0100

    Display sample text

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import board
 from busio import I2C
 from displayio import Group, I2CDisplay, release_displays
 
+from adafruit_bitmap_font.bitmap_font import load_font
+from adafruit_display_text.label import Label
 from adafruit_displayio_sh1106 import SH1106
 
 DISPLAY_ADDRESS = 0x3C
@@ -10,6 +12,9 @@ DISPLAY_SCL = board.GP15
 DISPLAY_SDA = board.GP14
 DISPLAY_WIDTH = 128
 DISPLAY_OFFSET_X = 2
+
+FONT_FILENAME = "/bizcat.bdf"
+FONT_HEIGHT = 16
 
 
 class Display:
@@ -30,9 +35,26 @@ class Display:
         self.display.show(Group())
         self.display.auto_refresh = True
 
+        self.font = load_font(FONT_FILENAME)
+
+    def update(self, text: str) -> None:
+        group = Group()
+
+        for idx, text_line in enumerate(text.splitlines()):
+            text_area = Label(self.font, text=text_line, color=0xFFFFFF)
+            text_area.x = 0
+            text_area.y = (FONT_HEIGHT // 2) + FONT_HEIGHT * idx
+
+            group.append(text_area)
+
+        self.display.show(group)
+
 
 if __name__ == "__main__":
+    text = "Motor avg speed:\n20%\n0123456789:01\n S+    S-    OK "
+
     display = Display()
+    display.update(text)
 
     while True:
         pass
