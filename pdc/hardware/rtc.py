a9040b6ead30d8dc6114a760e66fb94c10fbf791
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-01-14 22:56:46 +0100

    Use DS3231 module instead of built-in RTC

--- pdc/hardware/rtc.py
+++ pdc/hardware/rtc.py
@@ -1,15 +1,25 @@
-from rtc import RTC
+import rtc
+from time import struct_time
 
-from adafruit_datetime import datetime
+from adafruit_bus_device.i2c_device import I2CDevice
+from adafruit_ds3231 import DS3231
+
+from pdc import config
+from pdc.hardware.i2c import i2c
+
+_rtc = None
 
 
-def init_clock() -> RTC:
-    clock = RTC()
-    clock.datetime = datetime(2000, 1, 1, 0, 0, 0).timetuple()
+def init_clock() -> None:
+    global _rtc
 
-    return clock
+    _rtc = DS3231(i2c)
+    _rtc.i2c_device = I2CDevice(i2c, config.I2C_ADDRESS_RTC)
+
+    rtc.set_time_source(_rtc)
+
+    # todo: handle rtc.lost_power (reset related settings and set new date?)
 
 
-def set_clock(dt: datetime) -> None:
-    clock = RTC()
-    clock.datetime = dt
+def set_clock(dt: struct_time) -> None:
+    _rtc.datetime = dt
