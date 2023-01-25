998c5a5d886bfc7f78e7415eae0b0c356a0b975e
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-01-25 01:54:36 +0100

    Convert sample scene into working menu

--- pdc2/scenes.py
+++ pdc2/scenes.py
@@ -2,50 +2,80 @@ import asyncio
 
 from pdc.hardware import display, keys
 
+LINE_CURSOR = 1
+LINE_COUNT = 5
+
 
 class Scene:
+    name = "Scene"
+
     def __init__(self, manager):
         self._manager = manager
-
-    def handle_event(self, event):
-        pass
-
-    def refresh_screen(self):
-        pass
-
-
-class MainScene(Scene):
-    def __init__(self, manager):
-        super().__init__(manager)
-
-        self.entries = [MockedScene1, MockedScene2, MockedScene3]
 
     def handle_event(self, event):
         self.refresh_screen()
 
     def refresh_screen(self):
-        display.update([
-            display.write(0, 0, str(self.entries[0])),
-            display.write(1, 0, str(self.entries[1])),
-            display.write(2, 0, str(self.entries[2])),
-        ])
+        display.update([display.write(0, 0, self.__class__.__name__)])
+
+
+class MenuScene(Scene):
+    def __init__(self, manager):
+        super().__init__(manager)
+
+        self.entries = []
+        self.cursor_position = 0
+
+    def handle_event(self, event):
+        if event.key_number == 1 and self.cursor_position > 0:
+            self.cursor_position -= 1
+        if event.key_number == 2 and self.cursor_position < len(self.entries) - 1:
+            self.cursor_position += 1
+
+        self.refresh_screen()
+
+    def refresh_screen(self):
+        data = [display.write(LINE_CURSOR, 0, "*")]
+
+        for idx in range(LINE_COUNT):
+            position = self.cursor_position - 1 + idx
+            if 0 <= position < len(self.entries):
+                data.append(display.write(idx, 1, self.entries[position].name))
+
+        display.update(data)
+
+
+class MainMenuScene(MenuScene):
+    def __init__(self, manager):
+        super().__init__(manager)
+        self.entries = [MockedScene1, MockedScene2, MockedScene3]
 
 
 class MockedScene1(Scene):
-    pass
+    name = "Mocked Scene 1"
 
 
 class MockedScene2(Scene):
-    pass
+    name = "Mocked Scene 2"
 
 
 class MockedScene3(Scene):
-    pass
+    name = "Mocked Scene 3"
 
 
 class SceneManager:
     def __init__(self):
-        self.current_scene = MainScene(self)
+        self._current_scene = None
+        self.current_scene = MainMenuScene(self)
+
+    @property
+    def current_scene(self):
+        return self._current_scene
+
+    @current_scene.setter
+    def current_scene(self, scene):
+        self._current_scene = scene
+        self._current_scene.refresh_screen()
 
     async def poll(self):
         keys_device = keys.device
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cp -rv pdc pdc2 code.py "/media/$USER/CIRCUITPY"
+rsync -ruv pdc pdc2 code.py "/media/$USER/CIRCUITPY"
