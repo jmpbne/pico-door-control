300f17915c3f2a21a7027268cef6a2cdb6e004ee
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-02-03 01:54:26 +0100

    Add safe datetime getter/setter

--- controller/hardware/rtc.py
+++ controller/hardware/rtc.py
@@ -9,11 +9,29 @@ i2c = I2C(config.I2C_SCL, config.I2C_SDA)
 rtc_internal = RTC()
 rtc_external = DS3231(i2c)
 
+# Store external RTC state in case it gets disconnected
+_lost_power = rtc_external.lost_power
+
 
 def init():
-    if rtc_external.lost_power:
+    if _lost_power:
         print("External RTC lost power")
         return
 
     print("Retrieving time from external RTC")
     rtc_internal.datetime = rtc_external.datetime
+
+
+def get_datetime():
+    if _lost_power:
+        return None
+
+    return rtc_internal.datetime
+
+
+def set_datetime(dt):
+    global _lost_power
+
+    rtc_internal.datetime = dt
+    rtc_external.datetime = dt
+    _lost_power = False
