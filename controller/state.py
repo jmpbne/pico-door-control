8b9eb0af5b83f1e9c286c9a637510d724dd54c47
jmpbne <118080320+jmpbne@users.noreply.github.com>
2023-02-05 02:35:50 +0100

    Load NVM data into state object

---
+++ controller/state.py
@@ -0,0 +1,20 @@
+from controller.hardware import nvm
+
+data = {}
+
+
+def load_state():
+    data.clear()
+    data.update(**nvm.read_nvm())
+
+
+def load_default_state():
+    data.clear()
+
+
+def load_state_safe():
+    try:
+        load_state()
+    except Exception as e:
+        print(f"Could not load settings because of {type(e)}")
+        load_default_state()
